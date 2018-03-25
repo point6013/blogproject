272ed641646a10f6c0d69a411dde5369d1fbcb5a
point6013 <huangmeng891211@126.com>
2018-03-25 21:04:20 +0800

    add pep8

--- comments/forms.py
+++ comments/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 from .models import Comment
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
-        model=Comment
-        fields=['name','email','url','text']
-
-
+        model = Comment
+        fields = ['name', 'email', 'url', 'text']
